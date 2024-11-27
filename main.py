404557d4dc97cb322265a8ef0adb43722717f6a4
FalisEPG <106707009+Falis-EPG@users.noreply.github.com>
2024-11-27 07:52:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 ##########################################################################
-genai.configure(api_key="AIzaSyDZ0q7N_pb6I6T4G_apVUtY6mGY9yqQWnw")
+genai.configure(api_key="GOOGLE_API_KEY")
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
         print(m.name)
